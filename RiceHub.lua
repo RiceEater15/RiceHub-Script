e35ce7d552e43346fb1da853ac844afce7d8d745
VietRiceLord <144397456+RiceEater15@users.noreply.github.com>
2025-03-11 08:58:13 -0500

    Update RiceHub.lua

--- RiceHub.lua
+++ RiceHub.lua
@@ -98,24 +98,3 @@ humanoid.JumpPower = jumpPower
 
    end,
 })
-
-local Button = MainTab:CreateButton({
-   Name = "Infinite Jump",
-   Callback = function()
-  local InfiniteJumpEnabled = true
-game:GetService("UserInputService").JumpRequest:connect(function()
-	if InfiniteJumpEnabled then
-		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
-	end
-end)
-local InfiniteJump = CreateButton("Infinite Jump: On", StuffFrame)
-InfiniteJump.Position = UDim2.new(0,10,0,130)
-InfiniteJump.Size = UDim2.new(0,150,0,30)
-InfiniteJump.MouseButton1Click:connect(function()
-	local state = InfiniteJump.Text:sub(string.len("Infinite Jump: ") + 1) -
-	local new = state == "Off" and "On" or state == "On" and "Off"
-	InfiniteJumpEnabled = new == "On"
-	InfiniteJump.Text = "Infinite Jump: " .. new
-end)
-   end,
-	})
