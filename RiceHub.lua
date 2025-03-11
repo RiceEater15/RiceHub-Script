86bf551a1dac4a8d3d8d6e71f37aa4d8474850a7
VietRiceLord <144397456+RiceEater15@users.noreply.github.com>
2025-03-11 11:47:32 -0500

    Update RiceHub.lua

--- RiceHub.lua
+++ RiceHub.lua
@@ -57,31 +57,28 @@ local Button = MainTab:CreateButton({
    end,
 })
 
-local InfiniteJumpEnabled = false -- Variable to track the state of Infinite Jump
-
 local Button = MainTab:CreateButton({
    Name = "Infinite Jump",
    Callback = function()
-      game:GetService("UserInputService").JumpRequest:Connect(function()
-         if InfiniteJumpEnabled then
-            game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState("Jumping")
-         end
-      end)
-      
-     
-      local InfiniteJump = CreateButton("Infinite Jump: Off", StuffFrame)
-      InfiniteJump.Position = UDim2.new(0, 10, 0, 130)
-      InfiniteJump.Size = UDim2.new(0, 150, 0, 30)
-      
-      InfiniteJump.MouseButton1Click:Connect(function()
-         local state = InfiniteJump.Text:sub(string.len("Infinite Jump: ") + 1) 
-         local new = state == "Off" and "On" or "Off"
-         
-         InfiniteJumpEnabled = new == "On" 
-         InfiniteJump.Text = "Infinite Jump: " .. new 
-      end)
+  local InfiniteJumpEnabled = true
+game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+	end
+end)
+local InfiniteJump = CreateButton("Infinite Jump: On", StuffFrame)
+InfiniteJump.Position = UDim2.new(0,10,0,130)
+InfiniteJump.Size = UDim2.new(0,150,0,30)
+InfiniteJump.MouseButton1Click:connect(function()
+	local state = InfiniteJump.Text:sub(string.len("Infinite Jump: ") + 1) 
+	local new = state == "Off" and "On" or state == "On" and "Off"
+	InfiniteJumpEnabled = new == "On"
+	InfiniteJump.Text = "Infinite Jump: " .. new
+end)
    end,
 })
+
+
 
 
 local MainSection = MainTab:CreateSection("Admin")
