633422604aec20647e3d568e9ceddefdaa9f2511
VietRiceLord <144397456+RiceEater15@users.noreply.github.com>
2025-03-11 18:00:12 -0500

    Update RiceHub.lua

--- RiceHub.lua
+++ RiceHub.lua
@@ -96,38 +96,40 @@ local Toggle = MainTab:CreateToggle({
    CurrentValue = false,
    Flag = "Toggle1",
    Callback = function(Value)
-       if Value then
-           _G.HeadSize = 7
-           _G.Disabled = (Value)
+      _G.HeadSize = 7
+_G.Disabled = false
 
-           game:GetService('RunService').RenderStepped:connect(function()
-               if _G.Disabled then
-                   for _, player in next, game:GetService('Players'):GetPlayers() do
-                       if player.Name ~= game:GetService('Players').LocalPlayer.Name then
-                           pcall(function()
-                               local head = player.Character:FindFirstChild("Head")
-                               if head then
-                                   head.Size = Vector3.new(_G.HeadSize, _G.HeadSize, _G.HeadSize)
-                                   head.Transparency = 1
-                                   head.BrickColor = BrickColor.new("Red")
-                                   head.Material = "Neon"
-                                   head.CanCollide = false
-                                   head.Massless = true
-                               end
-                           end, function(err)
-                               print("Error: " .. err)
-                           end)
-                       end
-                   end
-               end
-           end)
-       else
-           _G.Disabled = false
-           print("Hitbox Expander Disabled")
-       end
+local Toggle = Tab:CreateToggle({
+   Name = "Head Size Toggle",
+   CurrentValue = _G.Disabled,
+   Flag = "Toggle1",
+   Callback = function(Value)
+       _G.Disabled = Value
    end,
 })
 
+game:GetService('RunService').RenderStepped:connect(function()
+    if _G.Disabled then
+        for _, player in next, game:GetService('Players'):GetPlayers() do
+            if player.Name ~= game:GetService('Players').LocalPlayer.Name then
+                pcall(function()
+                    local head = player.Character:FindFirstChild("Head")
+                    if head then
+                        head.Size = Vector3.new(_G.HeadSize, _G.HeadSize, _G.HeadSize)
+                        head.Transparency = 1
+                        head.BrickColor = BrickColor.new("Red")
+                        head.Material = "Neon"
+                        head.CanCollide = false
+                        head.Massless = true
+                    end
+                end, function(err)
+                    print("Error: " .. err)
+                end)
+            end
+        end
+    end
+end)
+		})
 
 
 local MainSection = MainTab:CreateSection("Admin")
