d41a68bcd2e46e40023cd583a6475de1ee5076f1
VietRiceLord <144397456+RiceEater15@users.noreply.github.com>
2025-03-12 19:54:31 -0500

    Update Arsenalinfammo.lua

--- Arsenalinfammo.lua
+++ Arsenalinfammo.lua
@@ -1,1 +1,20 @@
+local replicationstorage = game.ReplicatedStorage
+
+while wait() do
+    game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
+    game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
+end
+
+local MenuIsToggled = true
+
+Basic.Visible = false
+MenuIsToggled = false
+
+local minimize = ts:Create(ping, TweenInfo.new(1), {TextTransparency = 0})
+minimize:Play()
+
+wait(3)
+
+local unominimize = ts:Create(ping, TweenInfo.new(1), {TextTransparency = 1})
+unominimize:Play()
 
