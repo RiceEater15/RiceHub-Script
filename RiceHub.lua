72d2d4a463b19491c70dd59af82b64cf03b02bce
VietRiceLord <144397456+RiceEater15@users.noreply.github.com>
2025-08-02 21:55:05 +0000

    Update RiceHub.lua

--- RiceHub.lua
+++ RiceHub.lua
@@ -180,7 +180,34 @@ local Slider = MainTab:CreateSlider({
    CurrentValue = 15,
    Flag = "Slider1",
    Callback = function(Value)
+_G.HeadSize = (Value)
+_G.Disabled = true
 
+game:GetService('RunService').RenderStepped:connect(function()
+    if _G.Disabled then
+        for _, player in next, game:GetService('Players'):GetPlayers() do
+            if player.Name ~= game:GetService('Players').LocalPlayer.Name then
+                pcall(function()
+                    local head = player.Character:FindFirstChild("Head")
+                    if head then
+                        head.Size = Vector3.new(_G.HeadSize, _G.HeadSize, _G.HeadSize)
+                        head.Transparency = 1
+                        head.BrickColor = BrickColor.new("Red")
+                        head.Material = "Neon"
+                        head.CanCollide = false
+                        head.Massless = true
+                    end
+                end, function(err)
+                    print("Error: " .. err)
+                end)
+            end
+        end
+    end
+end)
+
+
+		end,
+	})
 
 local HubsTab = Window:CreateTab("Fisch", 4483362458 ) -- Title, Image
 local HubsSection = HubsTab:CreateSection("Fisch")
